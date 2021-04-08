32cc7d7defea46d0d72cffbe1886fda6810972d9
Mark Smith <smithmark.ms3@gmail.com>
2021-04-08 21:01:39 +0100

    optimised "require.php" staement and renaming

--- public/splash_tutorial.php
+++ public/splash_tutorial.php
@@ -3,13 +3,10 @@ session_start(); //used to start the session - has to be on the first line
 if (isset($_SESSION["admin"])) { //checks that the user is logged in
 	$title = ''; //sets the title
 	$content = '';//sets the content inside the page
-	require '../layout.php'; //gets content.php which provides the layout. It is outside of the public folder as users should not directly access it
 }
 else { //displays an access denied if the user is not logged in
   $title = 'Access denied';
   $content = 'Acess denied';
-  require '../layout.php';
 }
+require '../layout.php'; //loads layout.php which contains the layout for the page
 ?>
-
-
