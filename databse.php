e0780c98466f8f48ecb4cd484798bb23cc82298e
Rootvi Pandya <pandyarootvi@gmail.com>
2021-04-21 12:12:57 +0100

    Update databse.php

--- databse.php
+++ databse.php
@@ -5,7 +5,6 @@ $username = 'student';
 $password = 'student';
 $schema = 'WUC_RMS';
 
-//establishing connection with MYSQL database using following code from lecture notes-topic 8, available on NILE 
 $pdo = new PDO('mysql:dbname=' . $schema . ';host=' . $server, $username, $password, 
            [ PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 ?>
